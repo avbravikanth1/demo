4aed8623f0a87fc507df9e835f1ec89942f08cdb
ravikanth <ravikanth.ado@gmail.com>
2022-06-20 17:23:40 +0530

    changed script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script to delete olf files
-sudo find /home/devrabbit/Desktop/ravi/july/test -type f ! -newermt '06/20/2022 10:00:00' -exec rm -f {} \;
+sudo -s find /home/devrabbit/Desktop/ravi/july/test -type f ! -newermt '06/20/2022 10:00:00' -exec rm -f {} \;
 echo "there is no files up to 20th"
